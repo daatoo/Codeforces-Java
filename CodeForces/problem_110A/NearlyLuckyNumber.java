27fadcc92a8adfada0eb9a3724a5d3375ae752d0
david <davit.cheishvili.06@gmail.com>
2023-11-25 21:21:21 +0400

    add next algorithm

---
+++ CodeForces/problem_110A/NearlyLuckyNumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class NearlyLuckyNumber {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        String number = input.nextLine();
+        int count = 0;
+        for(int i = 0; i < number.length(); i++){
+            if(number.charAt(i) == '4' || number.charAt(i) == '7'){
+                count++;
+            }
+        }
+        if(count == 4 || count == 7){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+    }
+}
