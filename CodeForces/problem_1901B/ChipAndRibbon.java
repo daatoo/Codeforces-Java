286fc182063048114821b583825583f4d88b9808
david <davit.cheishvili.06@gmail.com>
2023-12-01 13:47:53 +0400

    delete other direcroties

---
+++ CodeForces/problem_1901B/ChipAndRibbon.java
@@ -0,0 +1,16 @@
+public class ChipAndRibbon {
+    public static void main(String[] args){
+        /*
+        5 4 3 2 1 -> 4
+        5 4 4 2 2 -> 4
+        5 4 0 2 1 -> 6
+
+        1 2 3 4 5 -> 4
+        1 2 2 3 5 -> 4
+
+        1 2 3 2 1 -> 2
+        1 1 3 1 1 -> 2
+        1 1 3 2 2 -> 2
+         */
+    }
+}
